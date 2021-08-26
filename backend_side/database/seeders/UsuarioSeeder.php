0c98864c4a6c2b129d6b2b94a33fcf09b9e6cb3f
Plem <pedromartinez19999@hotmail.com>
2021-08-25 20:16:35 -0500

    Seeder de usuarios

---
+++ backend_side/database/seeders/UsuarioSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+//Modelo 'mensajeria'
+use App\Models\User;
+//Utilizamos la clase 'Hash' para encriptar a los usuarios
+use Illuminate\Support\Facades\Hash;
+
+class UsuarioSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Creacion de Mensajerias ficticias
+        for($i=0;$i<10;$i++){
+            User::create([
+                'name' => 'Usuario ' . $i + 1,
+                'email' => 'correo' . $i . '@gmail.com',
+                'password' => Hash::make('correo'.$i.'@gmail.com')
+            ]);
+        }
+    }
+}
