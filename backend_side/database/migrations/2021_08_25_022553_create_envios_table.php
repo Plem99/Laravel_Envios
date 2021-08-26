01dc4541c447b47bafe81c9b3a17beb67a00a1b3
Plem <pedromartinez19999@hotmail.com>
2021-08-25 20:18:39 -0500

    Se agrego la llav del id_usuario

--- backend_side/database/migrations/2021_08_25_022553_create_envios_table.php
+++ backend_side/database/migrations/2021_08_25_022553_create_envios_table.php
@@ -22,8 +22,10 @@ class CreateEnviosTable extends Migration
             $table->double('alto', 5, 2);
             $table->double('ancho', 5, 2);
             $table->double('tarifa', 10, 2);
+            $table->bigInteger('id_usuario')->unsigned();
             $table->integer('id_mensajeria')->unsigned();
             //Llave foránea de la tabla 'mensajerias'
+            $table->foreign('id_usuario')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('id_mensajeria')->references('id')->on('mensajerias')->onDelete('cascade');
             $table->timestamps();
         });
