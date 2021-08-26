1727f73620055475537807a2a8a87ad623c015e9
Plem <pedromartinez19999@hotmail.com>
2021-08-25 20:18:06 -0500

    Se registra envio

--- backend_side/app/Http/Controllers/EnvioController.php
+++ backend_side/app/Http/Controllers/EnvioController.php
@@ -2,43 +2,63 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 //Modelo de 'envio'
 use App\Models\envio;
-//Modelo de 'rastreo'
-use App\Models\rastreo;
-//Modelo de 'mensajeria'
-use App\Models\mensajeria;
+use Illuminate\Http\Request;
 
 class EnvioController extends Controller
 {
+    //Creamos la variable para despues instanciar el objeto del controlador 'RastreoController'
+    private $rastreoObj;
+    //Creamos la variable para despues instanciar el objeto del controlador 'UsuarioController'
+    private $usuarioObj;
+
+    /**
+     * Constuctor para instanciar objetos de otros controladores
+     */
+    public function __construct()
+    {
+        //Instanciamos nuestro objeto del controlador 'RastreoController'
+        $this->rastreoObj = new RastreoController;
+        //Instanciamos nuestro objeto del controlador 'UsuarioController'
+        $this->usuarioObj = new UsuarioController;
+    }
+
     /**
      * Crea un nuevo envio con los datos enviados.
      *
      * @return \Illuminate\Http\Request  $request
      */
-    public function create(Request $request)
+    public function registrarEnvio(Request $request)
     {
         //Obtenemos el peso volumetrico
         $pesoVolumetrico = $this->pesoVolumetrico($request->largo, $request->alto, $request->ancho);
         //Calculamos la tarifa del envio redondeandola a 2 cifras significativas
         $tarifaEnvio = round($this->tarifaEnvio(intval($request->cpOrigen), intval($request->cpDestino), $request->peso, $pesoVolumetrico), 2);
-        //Generamos el código de rastreo
-        $codigoRastreo = $this->codigoRastreo($request);
         //Creamos nuestro registro en nuestra base de datos
-        // $registroEnvio = envio::create([
-        //     'cpOrigen' => $request->cpOrigen,
-        //     'cpDestino' => $request->cpDestino,
-        //     'peso' => $request->peso,
-        //     'largo' => $request->largo,
-        //     'alto' => $request->alto,
-        //     'ancho' => $request->ancho,
-        //     'tarifa' => $tarifaEnvio,
-        //     'id_mensajeria' => $request->id_mensajeria
-        // ]);
-        //return response()->json(['Registro Hecho' => $registroEnvio], 201);
+        $registroEnvio = envio::create([
+            'cpOrigen' => $request->cpOrigen,
+            'cpDestino' => $request->cpDestino,
+            'peso' => $request->peso,
+            'largo' => $request->largo,
+            'alto' => $request->alto,
+            'ancho' => $request->ancho,
+            'tarifa' => $tarifaEnvio,
+            'id_usuario' => $request->id_usuario,
+            'id_mensajeria' => $request->id_mensajeria
+        ]);
+        //Obtenemos el objeto de rastreo creado
+        $datosRastreo = $this->rastreoObj->codigoRastreo($registroEnvio);
+        //Obtenemos el objeto de usuario usado
+        $usuario = $this->usuarioObj->obtenerUsuario($request->id_usuario);
         //Retornamos la respuesta de nuestro registro de envío
-        return response()->json(['Registro Hecho' => $codigoRastreo], 201);
+        return response()->json([
+            'Usuario' => $usuario->name,
+            'Código de Rastreo' => $datosRastreo->original['codigo'],
+            'Mensajeria' => $datosRastreo->original['mensajeria'],
+            'Estado del Envío' => $datosRastreo->original['estado'],
+            'Tarifa de Envío (MXN)' => $tarifaEnvio
+        ], 201);
     }
 
     /**
@@ -59,34 +79,7 @@ class EnvioController extends Controller
      */
     public function tarifaEnvio($cpOrigen, $cpDestino, $peso, $pesoVolumetrico)
     {
-        return (($cpOrigen / 1000) + ($cpDestino / 1000) + ($peso)) * $pesoVolumetrico;
-    }
-
-    /**
-     * Generar código de rastreo.
-     * de 20 caracteres.
-     */
-    public function codigoRastreo($envio)
-    {
-        //Obtenemos la mensajeria con el id del que hayamos elegido
-        $mensajeria = mensajeria::findOrFail($envio->id_mensajeria);
-        //Eliminamos los espacios en blanco con 'preg_replace' con el pátron '/\s+/' para eliminar espacios en blanco
-        $codigoMensajeria = preg_replace('/\s+/', '', substr($mensajeria->nombre, 0, 3) . substr($mensajeria->nombre, -3) );
-        //Se concatena el cpOrigen + cpDestino + codigoMensajeria
-        $codigoRastreo = $envio->cpOrigen . $envio->cpDestino . $codigoMensajeria;
-        //y si hacen falta caracteres llenarlos de forma aleatoria hasta cumplir con el numero de 20
-        if(strlen($codigoRastreo) != 20 ){
-            if(strlen($codigoRastreo) < 20){
-                //convertimos a un valor absoluto los numero faltantes
-                $numerosFaltantes = abs(strlen($codigoRastreo) - 20);
-                return $codigoRastreo . rand($numerosFaltantes, $numerosFaltantes);
-            }else if(strlen($codigoRastreo) > 20){
-
-            }
-        }else{
-            return $codigoRastreo;
-        }
-
+        return (($cpOrigen / 1000) + ($cpDestino / 1000) + $peso) * $pesoVolumetrico;
     }
 
 }
