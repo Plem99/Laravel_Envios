45e50b53bdd01e0feb9e30cad57ad22bc7424337
Plem <pedromartinez19999@hotmail.com>
2021-08-25 20:19:33 -0500

    Se agrega un registro de rastreo

--- backend_side/app/Http/Controllers/RastreoController.php
+++ backend_side/app/Http/Controllers/RastreoController.php
@@ -2,29 +2,106 @@
 
 namespace App\Http\Controllers;
 
+//Modelo de 'rastreo'
 use App\Models\rastreo;
 use Illuminate\Http\Request;
 
 class RastreoController extends Controller
 {
+    //Creamos la variable para despues instanciar el objeto del controlador 'MensajeriaController'
+    private $mensajeriaObj;
+
+    //Caracteres necesarios para el codigo de rastreo
+    private const VALOR_CODIGO = 20; 
+
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
+     * Constuctor para instanciar objetos de otros controladores
      */
-    public function index()
+    public function __construct()
     {
-        //
+        //Instanciamos nuestro objeto del controlador 'MensajeriaController'
+        $this->mensajeriaObj = new MensajeriaController;
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
+     * Generar código de rastreo de 20 caracteres.
+     * Esperamos un object en donde estara nuestro registro de envio.
      */
-    public function create()
+    public function codigoRastreo($registroEnvio)
     {
-        return response()->json('Si entra', 201);
+        //Obtenemos la mensajeria con el id del que hayamos elegido
+        $mensajeria = $this->mensajeriaObj->obtenerMensajeria($registroEnvio->id_mensajeria);
+        //Eliminamos los espacios en blanco con 'preg_replace()' con el pátron '/\s+/' para eliminar espacios en blanco y,
+            //concatenamos parte del nombre de la mensajeria elegida
+        $codigoMensajeria = preg_replace('/\s+/', '', substr($mensajeria->nombre, 0, 3) . substr($mensajeria->nombre, -3) );
+        //Se concatena el cpOrigen + cpDestino + codigoMensajeria
+        $codigoRastreo = $registroEnvio->cpOrigen . $registroEnvio->cpDestino . $codigoMensajeria;
+        //Hacemos un ciclo para verificar que el codigo de rastreo sea valido
+        while(true){
+            //Guardamos el valor obtenido por la validacion
+            $codigoRastreoTemporal = $this->validarCodigoRastreo($codigoRastreo);
+            if($codigoRastreoTemporal){ //En el caso de que el valor no se encuentre en la base de datos rompe el ciclo
+                break;
+            }
+        }
+        //Guardamos el valor final del codigo en nuestra variable local
+        $codigoRastreo = $codigoRastreoTemporal;
+        //Guardamos nuestro registro de rastreo del envio
+        $registroRastreo = rastreo::create([
+            'codigo' => $codigoRastreo,
+            'estado' => 'En espera',
+            'id_envio' => $registroEnvio->id
+        ]);
+        //Retornamos el valor del registro del rastreo
+        return response([
+            'codigo' => $registroRastreo->codigo, 
+            'estado' => $registroRastreo->estado,
+            'mensajeria' => $mensajeria->nombre
+        ]);
+        
+
     }
 
+    /**
+     * Validamos si el código de rastreo es apto.
+     */
+    public function validarCodigoRastreo($codigoRastreo)
+    {
+        //Si hacen falta caracteres llenarlos de forma aleatoria hasta cumplir con el numero de 20
+        if(strlen($codigoRastreo) != self::VALOR_CODIGO ){
+            if(strlen($codigoRastreo) < self::VALOR_CODIGO){    //En el caso de ser menor a 20 caracteres
+                //Convertimos a un valor absoluto los numeros faltantes
+                $numerosFaltantes = abs(strlen($codigoRastreo) - self::VALOR_CODIGO);
+                //Va a dar la cantidad necesaria para calcular los 20 caracteres
+                for($i = self::VALOR_CODIGO - $numerosFaltantes; $i < self::VALOR_CODIGO; $i++){
+                    //Usamos el método 'str_pad()' para rellenar el string con un numero de caracteres definidos y,
+                        //generamos los numeros aleatorios
+                    $codigoRastreo = str_pad($codigoRastreo,  $i+1, strval(mt_rand(0, 9))); //Vamos sumando los numeros necesarios a rellenar hasta el 20
+                }
+            }else if(strlen($codigoRastreo) > self::VALOR_CODIGO){  //En el caso de ser mayor a 20 caracteres
+                //Solo recortamos la cadena de texto hasta 20 caracteres
+                $codigoRastreo = substr($codigoRastreo, 0, self::VALOR_CODIGO);
+            }
+            //Usamos una condicion ternaria para validar si el valor calculado no esta registrado en la base de datos,
+                //esto con el fin de evitar duplicidades
+            return !rastreo::where('codigo', '=', $codigoRastreo)->first('codigo') ? $codigoRastreo : false;
+        }else{
+            return !rastreo::where('codigo', '=', $codigoRastreo)->first('codigo') ? $codigoRastreo : false;
+        }
+    }
+
+    /**
+     * Rastreamos el envio por medio del codigo de rastreo.
+     * 
+     * @return \Illuminate\Http\Request  $request
+     */
+    public function rastrearEnvio(Request $request){
+        $rastreoEnvio = rastreo::where('codigo', '=', $request->codigo)->first();
+        return response()->json([
+            'Código de Rastreo' => $rastreoEnvio->codigo,
+            'Estado del Envío' => $rastreoEnvio->estado,
+            'Registro Creado' => $rastreoEnvio->created_at,
+            'Registro Actualizado' => $rastreoEnvio->updated_at
+        ], 201);
+    }
 }
